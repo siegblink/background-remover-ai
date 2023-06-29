a561b9ad86216309a7083062f104026522cc93f2
Siegfred Balona <siegblink@gmail.com>
2023-06-30 00:53:33 +0800

    feat: add types file

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export type NavItem = {
+  name: string;
+  href: string;
+  current: boolean;
+  icon: React.ForwardRefExoticComponent<
+    Omit<React.SVGProps<SVGSVGElement>, 'ref'> & {
+      title?: string | undefined;
+      titleId?: string | undefined;
+    } & React.RefAttributes<SVGSVGElement>
+  >;
+};
