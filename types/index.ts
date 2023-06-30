c0b720139d4690c08c88f645796ae7f2c4614da8
Siegfred Balona <siegblink@gmail.com>
2023-06-30 20:19:50 +0800

    refactor: update nav item type

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,6 @@
 export type NavItem = {
   name: string;
   href: string;
-  current: boolean;
   icon: React.ForwardRefExoticComponent<
     Omit<React.SVGProps<SVGSVGElement>, 'ref'> & {
       title?: string | undefined;
