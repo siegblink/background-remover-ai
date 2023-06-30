155cb7eab796aaf796f6c5dc02b90260cca2e48d
Siegfred Balona <siegblink@gmail.com>
2023-06-30 22:22:26 +0800

    feat: update root layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,22 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Background Remover AI',
+  description: 'Remove the background from any image with a single click.',
+  robots: 'index, follow',
   openGraph: {
     title: 'Background Remover AI',
     description: 'Remove the background from any image with a single click.',
-    images: ['/app-screenshot.png'],
+    url: 'https://background-remover-ai.vercel.app',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://background-remover-ai.vercel.app/app-screenshot.png',
+        width: 1200,
+        height: 630,
+        alt: 'Screenshot of the Background Remover AI app',
+      },
+    ],
   },
 };
 
